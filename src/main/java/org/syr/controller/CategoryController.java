bdbee5089b2709e8cf1d4ddc556ea1fdfee30376
nian0122 <3048586944@qq.com>
2024-04-23 23:12:01 +0800

    添加分组修正

--- src/main/java/org/syr/controller/CategoryController.java
+++ src/main/java/org/syr/controller/CategoryController.java
@@ -15,7 +15,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
     @PostMapping
-    public Result add(@RequestBody @Validated Category category){
+    public Result add(@RequestBody @Validated(Category.Add.class) Category category){
         categoryService.add(category);
         return Result.success();
     }
@@ -30,7 +30,7 @@ public class CategoryController {
         return Result.success(c);
     }
     @PutMapping
-    public Result update(@RequestBody @Validated Category category){
+    public Result update(@RequestBody @Validated(Category.Update.class) Category category){
         categoryService.update(category);
         return Result.success();
     }
--- src/main/java/org/syr/pojo/Category.java
+++ src/main/java/org/syr/pojo/Category.java
@@ -5,19 +5,28 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
-    @NotEmpty
+    @NotEmpty(groups = {Add.class, Update.class})
     private String categoryName;//分类名称
-    @NotEmpty
+    @NotEmpty(groups = {Add.class, Update.class})
     private String categoryAlias;//分类别名
     private Integer createUser;//创建人ID
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;//创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
+
+
+    public interface Add{
+
+    }
+    public interface Update{
+
+    }
 }
